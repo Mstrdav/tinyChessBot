5822e5a76e68fadaeb9ed0c007e45c3fd8282e0d
Mstrdav <colin.geindre@gmail.com>
2023-08-10 13:30:15 +0200

    restore evil bot and put new version in mybot

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,137 +1,54 @@
-﻿// using ChessChallenge.API;
-// using System;
-
-// namespace ChessChallenge.Example
-// {
-//     // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-//     // Plays randomly otherwise.
-//     public class EvilBot : IChessBot
-//     {
-//         // Piece values: null, pawn, knight, bishop, rook, queen, king
-//         int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-
-//         public Move Think(Board board, Timer timer)
-//         {
-//             Move[] allMoves = board.GetLegalMoves();
-
-//             // Pick a random move to play if nothing better is found
-//             Random rng = new();
-//             Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-//             int highestValueCapture = 0;
-
-//             foreach (Move move in allMoves)
-//             {
-//                 // Always play checkmate in one
-//                 if (MoveIsCheckmate(board, move))
-//                 {
-//                     moveToPlay = move;
-//                     break;
-//                 }
-
-//                 // Find highest value capture
-//                 Piece capturedPiece = board.GetPiece(move.TargetSquare);
-//                 int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-//                 if (capturedPieceValue > highestValueCapture)
-//                 {
-//                     moveToPlay = move;
-//                     highestValueCapture = capturedPieceValue;
-//                 }
-//             }
-
-//             return moveToPlay;
-//         }
-
-//         // Test if this move gives checkmate
-//         bool MoveIsCheckmate(Board board, Move move)
-//         {
-//             board.MakeMove(move);
-//             bool isMate = board.IsInCheckmate();
-//             board.UndoMove(move);
-//             return isMate;
-//         }
-//     }
-// }
-
-// Colin's Version
-using ChessChallenge.API;
+﻿using ChessChallenge.API;
 using System;
 
 namespace ChessChallenge.Example
 {
+    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
+    // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-
+        // Piece values: null, pawn, knight, bishop, rook, queen, king
         int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-        PlayerColor botColor;
-        int depthMax = 4;
-        int minEval = -10_000;
-        int maxEval = 10_000;
-        //Dictionary<ulong,int> evaluatedPositions = new Dictionary<ulong,int>();
 
-        int Evaluation(Board board, PlayerColor myBotColor)
-        {
-            int evaluation = 0;
-            if (board.IsDraw())
-                return evaluation;
-
-            return (int)myBotColor * board.GetLegalMoves().Length;
-        }
-
-        T Minimax<T>(Board board, int depth, string player, PlayerColor myBotColor, int alpha, int beta)
-        {
-            Move[] moves = board.GetLegalMoves();
-            if (depth == depthMax)
-                return (T)Convert.ChangeType(Evaluation(board, myBotColor), typeof(T));
-
-            else if (player == "BOT")
-            {
-                int bestEvaluation = minEval;
-                Move bestMove = new Move();
-                foreach (Move move in moves)
-                {
-                    board.MakeMove(move);
-                    int minimaxSon = Minimax<int>(board, depth + 1, "ADV", myBotColor, alpha, beta);
-                    if (minimaxSon > bestEvaluation)
-                    {
-                        bestEvaluation = minimaxSon;
-                        bestMove = move;
-                    }
-                    alpha = Math.Max(alpha, bestEvaluation);
-                    board.UndoMove(move);
-                    if (beta <= alpha)
-                        break;
-                }
-                if (depth == 0)
-                    return (T)Convert.ChangeType(bestMove, typeof(T));
-                return (T)Convert.ChangeType(bestEvaluation, typeof(T));
-            }
-
-            else
-            {
-                int worstEvaluation = maxEval;
-                foreach (Move move in moves)
-                {
-                    board.MakeMove(move);
-                    int minimaxSon = Minimax<int>(board, depth + 1, "BOT", myBotColor, alpha, beta);
-                    if (minimaxSon < worstEvaluation)
-                        worstEvaluation = minimaxSon;
-                    beta = Math.Min(beta, worstEvaluation);
-                    board.UndoMove(move);
-                    if (beta <= alpha)
-                        break;
-                }
-                return (T)Convert.ChangeType(worstEvaluation, typeof(T));
-            }
-        }
         public Move Think(Board board, Timer timer)
         {
-            //adapter prof au temps restant et au precedent temps de calcul
-            if (board.IsWhiteToMove)
-                botColor = PlayerColor.White;
-            else
-                botColor = PlayerColor.Black;
-            return Minimax<Move>(board, 0, "BOT", botColor, minEval, maxEval);
+            Move[] allMoves = board.GetLegalMoves();
+
+            // Pick a random move to play if nothing better is found
+            Random rng = new();
+            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
+            int highestValueCapture = 0;
+
+            foreach (Move move in allMoves)
+            {
+                // Always play checkmate in one
+                if (MoveIsCheckmate(board, move))
+                {
+                    moveToPlay = move;
+                    break;
+                }
+
+                // Find highest value capture
+                Piece capturedPiece = board.GetPiece(move.TargetSquare);
+                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+
+                if (capturedPieceValue > highestValueCapture)
+                {
+                    moveToPlay = move;
+                    highestValueCapture = capturedPieceValue;
+                }
+            }
+
+            return moveToPlay;
+        }
+
+        // Test if this move gives checkmate
+        bool MoveIsCheckmate(Board board, Move move)
+        {
+            board.MakeMove(move);
+            bool isMate = board.IsInCheckmate();
+            board.UndoMove(move);
+            return isMate;
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,89 +8,165 @@ public enum PlayerColor
 }
 
 
+// public class MyBot : IChessBot
+// {
+
+//     int[] pieceValues = {0,100,300,300,500,900,10000};
+//     PlayerColor botColor;
+//     int depthMax = 4;
+//     int minEval = -10_000;
+//     int maxEval =  10_000;
+//     //Dictionary<ulong,int> evaluatedPositions = new Dictionary<ulong,int>();
+
+//     int Evaluation(Board board, PlayerColor myBotColor)
+//     {
+//         int evaluation = 0;
+//         if(board.IsDraw())
+//             return evaluation;
+        
+//         PieceList[] allPieceLists = board.GetAllPieceLists();
+//         foreach(PieceList pieceList in allPieceLists)
+//         {
+//             int pieceNumber = pieceList.Count;
+//             PieceType pieceType = pieceList.TypeOfPieceInList;
+//             int piecePoints = pieceValues[(int)pieceType]*pieceNumber;
+//             if(pieceList.IsWhitePieceList)
+//                 evaluation += piecePoints;
+//             else
+//                 evaluation -= piecePoints;
+//         }
+//         return (int)myBotColor*evaluation;
+//     }
+
+//     T Minimax<T>(Board board, int depth,string player, PlayerColor myBotColor, int alpha, int beta) 
+//     {
+//         Move[] moves = board.GetLegalMoves();
+//         if(depth == depthMax)
+//             return (T) Convert.ChangeType(Evaluation(board,myBotColor),typeof(T));
+
+//         else if(player == "BOT")
+//         {
+//             int bestEvaluation = minEval;
+//             Move bestMove = new Move();
+//             foreach(Move move in moves)
+//             {
+//                 board.MakeMove(move);
+//                 int minimaxSon = Minimax<int>(board,depth+1,"ADV",myBotColor,alpha,beta);
+//                 if(minimaxSon > bestEvaluation)
+//                 {
+//                     bestEvaluation = minimaxSon;
+//                     bestMove = move;
+//                 }
+//                 alpha = Math.Max(alpha,bestEvaluation);
+//                 board.UndoMove(move);
+//                 if(beta<=alpha)
+//                     break;
+//             }
+//             if(depth == 0)
+//                 return (T) Convert.ChangeType(bestMove,typeof(T));
+//             return (T) Convert.ChangeType(bestEvaluation,typeof(T));
+//         }
+
+//         else
+//         {
+//             int worstEvaluation = maxEval;
+//             foreach(Move move in moves)
+//             {
+//                 board.MakeMove(move);
+//                 int minimaxSon = Minimax<int>(board,depth+1,"BOT",myBotColor,alpha,beta);
+//                 if(minimaxSon < worstEvaluation)
+//                     worstEvaluation = minimaxSon;
+//                 beta = Math.Min(beta,worstEvaluation);
+//                 board.UndoMove(move);
+//                 if(beta <= alpha)
+//                     break;
+//             }
+//             return (T) Convert.ChangeType(worstEvaluation,typeof(T));
+//         }
+//     }
+//     public Move Think(Board board, Timer timer)
+//     {
+//         //adapter prof au temps restant et au precedent temps de calcul
+//         if(board.IsWhiteToMove)
+//             botColor = PlayerColor.White;
+//         else 
+//             botColor = PlayerColor.Black;
+//         return Minimax<Move>(board,0,"BOT",botColor,minEval,maxEval);
+//     }
+// }
+
 public class MyBot : IChessBot
 {
-
-    int[] pieceValues = {0,100,300,300,500,900,10000};
     PlayerColor botColor;
-    int depthMax = 4;
+    int depthMax = 10;
+    int actualDepth = 0;
     int minEval = -10_000;
-    int maxEval =  10_000;
+    int maxEval = 10_000;
     //Dictionary<ulong,int> evaluatedPositions = new Dictionary<ulong,int>();
 
     int Evaluation(Board board, PlayerColor myBotColor)
     {
-        int evaluation = 0;
-        if(board.IsDraw())
-            return evaluation;
-        
-        PieceList[] allPieceLists = board.GetAllPieceLists();
-        foreach(PieceList pieceList in allPieceLists)
-        {
-            int pieceNumber = pieceList.Count;
-            PieceType pieceType = pieceList.TypeOfPieceInList;
-            int piecePoints = pieceValues[(int)pieceType]*pieceNumber;
-            if(pieceList.IsWhitePieceList)
-                evaluation += piecePoints;
-            else
-                evaluation -= piecePoints;
-        }
-        return (int)myBotColor*evaluation;
+        return (int)myBotColor * board.GetLegalMoves().Length;
     }
 
-    T Minimax<T>(Board board, int depth,string player, PlayerColor myBotColor, int alpha, int beta) 
+    T Minimax<T>(Board board, int depth, string player, PlayerColor myBotColor, int alpha, int beta)
     {
         Move[] moves = board.GetLegalMoves();
-        if(depth == depthMax)
-            return (T) Convert.ChangeType(Evaluation(board,myBotColor),typeof(T));
+        if (depth == actualDepth)
+            return (T)Convert.ChangeType(Evaluation(board, myBotColor), typeof(T));
 
-        else if(player == "BOT")
+        else if (player == "BOT")
         {
             int bestEvaluation = minEval;
             Move bestMove = new Move();
-            foreach(Move move in moves)
+            foreach (Move move in moves)
             {
                 board.MakeMove(move);
-                int minimaxSon = Minimax<int>(board,depth+1,"ADV",myBotColor,alpha,beta);
-                if(minimaxSon > bestEvaluation)
+                int minimaxSon = Minimax<int>(board, depth + 1, "ADV", myBotColor, alpha, beta);
+                if (minimaxSon > bestEvaluation)
                 {
                     bestEvaluation = minimaxSon;
                     bestMove = move;
                 }
-                alpha = Math.Max(alpha,bestEvaluation);
+                alpha = Math.Max(alpha, bestEvaluation);
                 board.UndoMove(move);
-                if(beta<=alpha)
+                if (beta <= alpha)
                     break;
             }
-            if(depth == 0)
-                return (T) Convert.ChangeType(bestMove,typeof(T));
-            return (T) Convert.ChangeType(bestEvaluation,typeof(T));
+            if (depth == 0)
+                return (T)Convert.ChangeType(bestMove, typeof(T));
+            return (T)Convert.ChangeType(bestEvaluation, typeof(T));
         }
 
         else
         {
             int worstEvaluation = maxEval;
-            foreach(Move move in moves)
+            foreach (Move move in moves)
             {
                 board.MakeMove(move);
-                int minimaxSon = Minimax<int>(board,depth+1,"BOT",myBotColor,alpha,beta);
-                if(minimaxSon < worstEvaluation)
+                int minimaxSon = Minimax<int>(board, depth + 1, "BOT", myBotColor, alpha, beta);
+                if (minimaxSon < worstEvaluation)
                     worstEvaluation = minimaxSon;
-                beta = Math.Min(beta,worstEvaluation);
+                beta = Math.Min(beta, worstEvaluation);
                 board.UndoMove(move);
-                if(beta <= alpha)
+                if (beta <= alpha)
                     break;
             }
-            return (T) Convert.ChangeType(worstEvaluation,typeof(T));
+            return (T)Convert.ChangeType(worstEvaluation, typeof(T));
         }
     }
     public Move Think(Board board, Timer timer)
     {
         //adapter prof au temps restant et au precedent temps de calcul
-        if(board.IsWhiteToMove)
+        if (board.IsWhiteToMove)
             botColor = PlayerColor.White;
-        else 
+        else
             botColor = PlayerColor.Black;
-        return Minimax<Move>(board,0,"BOT",botColor,minEval,maxEval);
+
+        actualDepth = depthMax * timer.MillisecondsRemaining / timer.GameStartTimeMilliseconds;
+        //round it
+        actualDepth = (int)Math.Round(actualDepth / 2.0) * 2;
+        actualDepth = Math.Max(1, actualDepth);
+        return Minimax<Move>(board, 0, "BOT", botColor, minEval, maxEval);
     }
 }
